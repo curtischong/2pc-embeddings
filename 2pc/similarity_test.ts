5717600718512e00ae7fd57567395722cf186e09
Curtis Chong <curtischong5@gmail.com>
2024-02-17 09:59:50 -0800

    wip 2pc

---
+++ 2pc/similarity_test.ts
@@ -0,0 +1,32 @@
+// tysm vitalik: https://vitalik.eth.limo/general/2020/03/21/garbled.html
+
+interface Profile {
+    embedding: number[];
+}
+
+let a:Profile = {
+    embedding: [1, 2, 3],
+};
+let b:Profile = {
+    embedding: [1, 1, 1]
+}
+
+let bit_len = 256;
+
+let intToBinary = (n:number) => {
+    let binary = n.toString(2);
+    return "0".repeat(bit_len - binary.length) + binary;
+}
+
+let pre2pc = (a:Profile) => {
+    let a_bin = a.embedding.map(intToBinary);
+    for(let i = 0; i < a_bin.length; i++){
+        let label = Math.random() > 0.5 ? 0 : 1;
+        a_bin[i] = a_bin[i] + label;
+    }
+
+
+    // first generate two labels
+
+
+}
