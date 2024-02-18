5e0021c18efd46adf29bc919b2ae1c45c94dc3e9
Curtis Chong <curtischong5@gmail.com>
2024-02-18 08:12:31 -0800

    testing works

--- webrtc/user_server/components/ToggleBeacon.tsx
+++ webrtc/user_server/components/ToggleBeacon.tsx
@@ -80,7 +80,7 @@ export default function ToggleBeacon() {
             let aliceUUID = "ERROR - hsould be filled";
             let bobUUID = "ERROR - hsould be filled";
             if(messageType == MessageType.AliceInit2pc){
-                localStorage.setItem('embedding', JSON.stringify([-1,-1,-1,-1]))
+                localStorage.setItem('embedding', JSON.stringify([1,1,1,0.5]))
                 localStorage.setItem('aliceUUID', message.uuid)
                 localStorage.setItem('bobUUID', message.target_uuid)
                 aliceUUID = message.uuid
